983586781aff2488521d06ebd754d4cb356741ba
Miklos Kaboldy <miklos.kaboldy@gmail.com>
2020-02-09 19:01:05 +0100

    Bugfix: show invalid shortcodes too

--- classes/class-wp-whatshortcode.php
+++ classes/class-wp-whatshortcode.php
@@ -68,29 +68,61 @@ if ( ! class_exists( 'WP_WhatShortCode' ) ) {
 			global $post;
 			if ( self::$column_id === $column ) {
 
-				$pattern = get_shortcode_regex();
- 
-				if ( preg_match_all( '/'. $pattern .'/s', $post->post_content, $matches )) {
+				// phpcs:disable Squiz.Commenting.InlineComment.InvalidEndChar
+				// phpcs:disable Squiz.Strings.ConcatenationSpacing.PaddingFound
+				// phpcs:disable Squiz.Strings.DoubleQuoteUsage.NotRequired
+				$pattern =
+						'\\['                            // Opening bracket
+						. '(\\[?)'                       // 1: Optional second opening bracket for escaping shortcodes: [[tag]]
+					. "([a-zA-Z0-9_-]*)"                 // 2: Shortcode name
+					. '(?![\\w-])'                       // Not followed by word character or hyphen
+					. '('                                // 3: Unroll the loop: Inside the opening shortcode tag
+					.     '[^\\]\\/]*'                   // Not a closing bracket or forward slash
+					.     '(?:'
+					.         '\\/(?!\\])'               // A forward slash not followed by a closing bracket
+					.         '[^\\]\\/]*'               // Not a closing bracket or forward slash
+					.     ')*?'
+					. ')'
+					. '(?:'
+					.     '(\\/)'                        // 4: Self closing tag ...
+					.     '\\]'                          // ... and closing bracket
+					. '|'
+					.     '\\]'                          // Closing bracket
+					.     '(?:'
+					.         '('                        // 5: Unroll the loop: Optionally, anything between the opening and closing shortcode tags
+					.             '[^\\[]*+'             // Not an opening bracket
+					.             '(?:'
+					.                 '\\[(?!\\/\\2\\])' // An opening bracket not followed by the closing shortcode tag
+					.                 '[^\\[]*+'         // Not an opening bracket
+					.             ')*+'
+					.         ')'
+					.         '\\[\\/\\2\\]'             // Closing shortcode tag
+					.     ')?'
+					. ')'
+					. '(\\]?)';                          // 6: Optional second closing brocket for escaping shortcodes: [[tag]]
+
+				if ( preg_match_all( '/' . $pattern . '/s', $post->post_content, $matches ) ) {
 					$shortcodes = array();
-					foreach ( $matches[2] as $tag) {
+					foreach ( $matches[2] as $tag ) {
 						$display_title = '';
 						$display_class = '';
 
-						if ( shortcode_exists($tag)) {
-					
-						} else {
+						if ( ! shortcode_exists( $tag ) ) {
 							$display_title = __( 'Shortcode doesn\'t exist' );
 							$display_class = 'notice notice-error';
 						}
-						
+
 						$shortcodes[] = sprintf(
-							'<span class="%1$s" title="%2$s"> %3$s </span>',
-							esc_html( $display_class ),
+							'<span id="%4s" class="%1$s" title="%2$s"> %3$s </span>',
+							$display_class,
 							esc_html( $display_title ),
-							esc_html($tag)
-						);		
+							esc_html( $tag ),
+							$post_id
+						);
 					}
-					echo implode(', ', $shortcodes);
+
+					// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+					echo implode( ', ', $shortcodes );
 				}
 			}
 		}
